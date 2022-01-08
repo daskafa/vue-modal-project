cb351d08b4cf701c68d6b57e9d1c6b2961f08de3
Yılmaz Daşkafa <ydaskafa@gmail.com>
2022-01-08 22:21:13 +0300

    video 5, start creating another modal

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,38 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ title }}</h1>
+  <p>Welcome...</p>
+  <div v-if="showModal">
+    <Modal theme="sale" @close="toggleModal">
+      <template v-slot:links>
+        <a href="#">sign up now</a>
+        <a href="#">more info</a>
+      </template>
+      <h2>Ninja Giveaway!</h2>
+      <p>Grab your ninja swag for half price!</p>
+    </Modal>
+  </div>
+  <button @click.alt="toggleModal">open modal(alt)</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Modal from './components/Modal.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Modal
+  },
+  data() {
+    return {
+      title: 'my first vue app :)',
+      header: 'sign up for the giveaway',
+      text: 'grab your ninja swag for half price!',
+      showModal: false
+    }
+  },
+  methods: {
+    toggleModal(){
+      this.showModal = !this.showModal
+    }
   }
 }
 </script>
@@ -22,5 +45,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
